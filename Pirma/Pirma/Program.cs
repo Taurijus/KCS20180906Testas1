96aedb0be73e926d99de5f5c92e057f564847718
LinasGr <linas.grabnickas@gmail.com>
2018-09-19 08:40:43 +0300

    Testas 1

--- Pirma/Pirma/Program.cs
+++ Pirma/Pirma/Program.cs
@@ -10,6 +10,10 @@ namespace Pirma
   {
     static void Main(string[] args)
     {
+      //Testas 1
+      Console.WriteLine("Hello world!");
+      Console.WriteLine("Tai yra mano pirmas testas.\nTikiuosi man jis pasiseks.");
+      Console.WriteLine("2+2="+(2+2)+"!");
     }
   }
 }
