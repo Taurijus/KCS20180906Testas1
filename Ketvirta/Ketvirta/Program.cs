1872e2ed6dab36f7705e49c74f57b12757f79fa6
LinasGr <linas.grabnickas@gmail.com>
2018-09-19 12:50:21 +0300

    Ketvirta uzduotis.

--- Ketvirta/Ketvirta/Program.cs
+++ Ketvirta/Ketvirta/Program.cs
@@ -10,6 +10,19 @@ namespace Ketvirta
   {
     static void Main(string[] args)
     {
+      //Ketvirta uzduotis
+      //Ivesta eilute issaugom mazosiom raidem
+      string str = Console.ReadLine().ToLower();
+      //Sukuriam char masyva eilutes dydzio
+      char[] strReverseToChar = new char[str.Length];
+      //Apverciam eilute i char masyva
+      for (int i = 0; i < str.Length; i++)
+      {
+        strReverseToChar[i] = str[str.Length - i-1];
+      }
+      //Lyginam pradine eilute su apverstu char masyvu ir isvedam atsakyma
+      if (str==new string(strReverseToChar)) Console.WriteLine(true);
+      else Console.WriteLine(false);
     }
   }
 }
